cee764486e442f7e7cf101eb4d70b9db1f4f2508
AmirrezaAsk <raskarpour@gmail.com>
2018-04-14 15:50:49 +0430

    workers and HeadWorker added

--- phone_gen.go
+++ phone_gen.go
@@ -7,6 +7,17 @@ import (
 	"strconv"
 )
 
+//var prefix = "/User/amirrezaask/Desktop/phones"
+
+type workerData struct {
+	start int
+	end   int
+}
+
+var workerPool = make(chan workerData, 900)
+var count = make(chan int, 1)
+var exit = make(chan int, 1)
+
 func writeToFile(path, text string) error {
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
 	if err != nil {
@@ -19,8 +30,21 @@ func writeToFile(path, text string) error {
 	}
 	return nil
 }
+func Work() {
+	w := <-workerPool
+	Generate(fmt.Sprintf("./output/%d_%d.txt", w.start, w.end), w.start, w.end)
+	count <- 1 + <-count
+}
+func HeadWorker() {
+	counter := <-exit
+	if counter == 900 {
+		exit <- 1
+		return
+	}
+	count <- counter
+	return
+}
 func Generate(filename string, start int, end int) {
-
 	for i := 0; i+start <= end; i++ {
 		writeToFile(filename, fmt.Sprintf("%d\n", start+i))
 	}
@@ -29,20 +53,33 @@ func Generate(filename string, start int, end int) {
 
 func main() {
 	counter := 0
-	start, err := strconv.Atoi(os.Args[1])
+	exit <- 1
+	prefix, err := strconv.Atoi(os.Args[1])
 	if err != nil {
-		log.Fatalf("Error First Paramter should be a number")
+		log.Fatalf("Argument should be cellphone prefix:%v", err)
 	}
-	end, err := strconv.Atoi(os.Args[2])
+	start, err := strconv.Atoi(fmt.Sprintf("%d1000000", prefix))
 	if err != nil {
-		log.Fatalf("Error Second Parameter should be a number")
+		log.Fatalf("Something went wrong :%v", err)
+	}
+	end, err := strconv.Atoi(fmt.Sprintf("%d9999999", prefix))
+	fmt.Println(end - start)
+
+	if err != nil {
+		log.Fatalf("Something went wrong :%v", err)
+	}
+	go HeadWorker()
+	for i := 0; i < 900; i++ {
+		go Work()
 	}
 	for i := 0; i+start <= end; i++ {
 		if counter%10000 == 0 {
 			log.Printf("Goroutine %v Started!!", counter/10000)
-			go Generate(fmt.Sprintf("%d_%d.txt", i+start, i+start+10000), i+start, i+start+10000)
+			workerPool <- workerData{start: i + start, end: i + end}
 		}
 		counter += 1
 	}
-
+	if <-exit == 1 {
+		return
+	}
 }
